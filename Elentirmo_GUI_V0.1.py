747047f5602082ddc7bce639a8aa22936be55a9c
kellogg76 <mathew.nightingale@gmail.com>
2016-12-09 13:05:29 -0400

    Update Elentirmo_GUI_V0.1.py
    altered usbport configuration

--- Elentirmo_GUI_V0.1.py
+++ Elentirmo_GUI_V0.1.py
@@ -10,128 +10,83 @@ dust_cover_text.set('Cover Closed')
 flat_box_text = StringVar()
 flat_box_text.set('Flat Box Off')
 
+# Assign Arduino's serial port address
+#   Windows example
+#     usbport = 'COM3'
+#   Linux example
+#     usbport = '/dev/ttyUSB0'
+#   MacOSX example
+#     usbport = '/dev/cu.usbmodemFA131'
+usbport = '/dev/cu.usbmodemFA131'
+
 def dust_cover_open():
     print "Opening"
     ## Open a serial connection with Arduino.
-
     import time
-
     import serial
-
-    ser = serial.Serial("COM9", 9600)   # Open serial port that Arduino is using
-
+    ser = serial.Serial(usbport, 9600)   # Open serial port that Arduino is using
     time.sleep(3)                       # Wait 3 seconds for Arduino to reset
-
     print ser                           # Print serial config
-
-
-
     print "Sending serial command to OPEN the dust cover."
     ser.write("O")
-
     print "Opening serial connection."
-
     ser.close()
     # Reminder to close the connection when finished
-
     if(ser.isOpen()):
-
      print "Serial connection is still open."
-
     dust_cover_label.config(bg="Green")
     dust_cover_text.set('Cover is Open')
 
 def dust_cover_close():
     print "Closing"
     ## Open a serial connection with Arduino.
-
     import time
-
     import serial
-
-    ser = serial.Serial("COM9", 9600)   # Open serial port that Arduino is using
-
+    ser = serial.Serial(usbport, 9600)   # Open serial port that Arduino is using
     time.sleep(3)                       # Wait 3 seconds for Arduino to reset
-
     print ser                           # Print serial config
-
-
-
     print "Sending serial command to CLOSE the dust cover."
     ser.write("C")
-
     print "Closing serial connection."
-
     ser.close()
-
     # Reminder to close the connection when finished
-
     if(ser.isOpen()):
-
      print "Serial connection is still open."
-
     dust_cover_label.config(bg="red")
     dust_cover_text.set('Cover is closed')
 
 def flat_on():
     print "Activating flat box"
     ## Open a serial connection with Arduino.
-
     import time
-
     import serial
-
     ser = serial.Serial("COM9", 9600)   # Open serial port that Arduino is using
-
     time.sleep(3)                       # Wait 3 seconds for Arduino to reset
-
     print ser                           # Print serial config
-
-
-
     print "Sending serial command to turn on the flat box via relay."
     ser.write("Q")
-
     print "Opening serial connection."
-
     ser.close()
-
     # Reminder to close the connection when finished
-
     if(ser.isOpen()):
-
      print "Serial connection is still open."
-
     flat_box_label.config(bg="Green")
     flat_box_text.set('Flat Box on')
 
 def flat_off():
     print "Dectivating flat box"
     ## Open a serial connection with Arduino.
-
     import time
-
     import serial
-
     ser = serial.Serial("COM9", 9600)   # Open serial port that Arduino is using
-
     time.sleep(3)                       # Wait 3 seconds for Arduino to reset
-
     print ser                           # Print serial config
-
-
-
     print "Sending serial command to turn off the flat box via relay."
     ser.write("F")
-
     print "Opening serial connection."
-
     ser.close()
-
     # Reminder to close the connection when finished
-
     if(ser.isOpen()):
-
      print "Serial connection is still open."
     flat_box_label.config(bg="red")
     flat_box_text.set('Flat Box Off')
